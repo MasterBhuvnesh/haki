63550de618b5b2d4f00212dc6b8dbd4f4f99f919
MasterBhuvnesh <bhuvneshverma2904@gmail.com>
2025-10-31 17:51:57 +0530

    test

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,6 +7,7 @@ function App(): React.JSX.Element {
     <>
       <div className="welcome">Welcome to GDG RBU</div>
       <div className="welcome">Testing 16:00</div>
+      <div className="welcome">3.7.0</div>
 
       <div className="action">
         <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
